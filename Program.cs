f77de44018aeb1d427250a61f24f817fd4cf7c07
kasturipratik <pratik.parajuli@yahoo.com>
2021-10-26 13:24:57 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HexaDecimalNumberSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] a = { "asd" , "sdad"};
+            double value = 0.00;
+            foreach(string i in a)
+            {
+                foreach (char j in i)
+                {
+                    value += Convert.ToInt32(j);
+                }
+            }
+
+            Console.WriteLine("sum Of two hexa decimal num:" + value);
+        }
+    }
+}
